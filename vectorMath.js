09e5d73bdcacac3efb01681c751f721d94b335c3
CarterKriessler <12605464v2@gmail.com>
2026-01-21 20:42:53 -0500

    Create vectorMath.js

---
+++ vectorMath.js
@@ -0,0 +1,11 @@
+(function (global) {
+    function cross(a, b) {
+        return {
+            x: a.y * b.z - a.z * b.y,
+            y: a.z * b.x - a.x * b.z,
+            z: a.x * b.y - a.y * b.x
+        };
+    }
+
+    global.vectorMath = { cross: cross };
+})(window);
