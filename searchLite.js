fcd6826159e1aee3afd5bd4e74036bff96bf1bc8
CarterKriessler <12605464v2@gmail.com>
2026-01-21 20:56:36 -0500

    Update searchLite.js

--- searchLite.js
+++ searchLite.js
@@ -1,28 +1,36 @@
 (function (global) {
-    async function search(query, limit = 3) {
-        const url =
-            "https://api.duckduckgo.com/?q=" +
-            encodeURIComponent(query) +
-            "&format=json&no_html=1&skip_disambig=1";
+    function search(query, limit = 3, callback) {
+        const cbName = "ddg_cb_" + Math.random().toString(36).substr(2);
 
-        const res = await fetch(url);
-        const data = await res.json();
+        global[cbName] = function (data) {
+            const results = [];
 
-        const results = [];
-
-        if (data.AbstractText) {
-            results.push(data.AbstractText);
-        }
-
-        if (data.RelatedTopics) {
-            for (let i = 0; i < data.RelatedTopics.length && results.length < limit; i++) {
-                const item = data.RelatedTopics[i];
-                if (item.Text) results.push(item.Text);
+            if (data.AbstractText) {
+                results.push(data.AbstractText);
             }
-        }
 
-        return results.slice(0, limit);
+            if (data.RelatedTopics) {
+                for (let i = 0; i < data.RelatedTopics.length && results.length < limit; i++) {
+                    const item = data.RelatedTopics[i];
+                    if (item.Text) results.push(item.Text);
+                }
+            }
+
+            callback(results.slice(0, limit));
+            delete global[cbName];
+        };
+
+        const script = document.createElement("script");
+        script.src =
+            "https://api.duckduckgo.com/?" +
+            "q=" + encodeURIComponent(query) +
+            "&format=json" +
+            "&no_html=1" +
+            "&skip_disambig=1" +
+            "&callback=" + cbName;
+
+        document.body.appendChild(script);
     }
 
-    global.searchLite = { search };
+    global.searchLite = { search: search };
 })(window);
